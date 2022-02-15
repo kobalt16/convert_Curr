dec0976db5a7190d4c0c07f475f234a92d06864b
Ruslan <96114211+kobalt16@users.noreply.github.com>
2022-02-15 08:54:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// fetch('https://www.cbr-xml-daily.ru/daily_json.js').then(function(result) {
+//     return result.json()
+// }).then(function (data) {
+//     console.log(data);
+// })
+
+//Объект курса валют
+const rates = {};
+//Информер
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const elementGBP = document.querySelector('[data-value="GBP"]');
+//Элементы формы
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
+const select = document.querySelector('#select');
+
+getCurrencies();
+
+async function getCurrencies () {
+    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    const data = await response.json();
+    const result = await data;
+    console.log(result);
+
+    rates.USD = result.Valute.USD;
+    rates.EUR = result.Valute.EUR;
+    rates.GBP = result.Valute.GBP;
+
+    console.log(rates);
+
+    elementUSD.textContent = rates.USD.Value.toFixed(2);
+    elementEUR.textContent = rates.EUR.Value.toFixed(2);
+    elementGBP.textContent = rates.GBP.Value.toFixed(2);
+
+    // Цвет для информеров
+    if (rates.USD.Value > rates.USD.Previous) {
+        elementUSD.classList.add('top');
+    } else {
+        elementUSD.classList.add('bottom');
+    }
+
+    if (rates.EUR.Value > rates.EUR.Previous) {
+        elementEUR.classList.add('top');
+    } else {
+        elementEUR.classList.add('bottom');
+    }
+
+    if (rates.GBP.Value > rates.GBP.Previous) {
+        elementGBP.classList.add('top');
+    } else {
+        elementGBP.classList.add('bottom');
+    }
+
+
+    
+}
+//Select
+input.oninput = convertValue;
+select.oninput = convertValue;
+
+function convertValue() {
+    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
+}
